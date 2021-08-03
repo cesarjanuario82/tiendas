fc483d6278b506744f51b41f4edee424788edb36
cesarjanuario82 <87102305+cesarjanuario82@users.noreply.github.com>
2021-08-03 14:18:35 -0500

    Update ofertas.js

--- ofertas.js
+++ ofertas.js
@@ -4,28 +4,28 @@
 "fechaVigencia": "2021-08-05 00:00:00",
 "diasVigencia": 3,
 "oferta": [
-{
-"topupAmount": "50",
-"dn": "5530301228",
-"tskId": "1212-13fh03-fsdr0-dg4546-fsds",
-"beneficiosBase": [
-{
-"benefitType": "Navegar",
-"benefitQuantity": "500 MB"
-},
-{
-"benefitType": "Llamadas/SMS",
-"benefitQuantity": "Ilimitado*"
-},
-{
-"benefitType": "Redes Sociales",
-"benefitQuantity": "Ilimitado**"
-}
-],
-"beneficiosOferta": {
-"benefitType": "Adicionales",
-"benefitQuantity": "500 MB"
-}
-}
-]
+    {
+    "topupAmount": "50",
+    "dn": "5530301228",
+    "tskId": "1212-13fh03-fsdr0-dg4546-fsds",
+    "beneficiosBase": [
+    {
+    "benefitType": "Navegar",
+    "benefitQuantity": "500 MB"
+    },
+    {
+    "benefitType": "Llamadas/SMS",
+    "benefitQuantity": "Ilimitado*"
+    },
+    {
+    "benefitType": "Redes Sociales",
+    "benefitQuantity": "Ilimitado**"
+    }
+    ],
+    "beneficiosOferta": {
+    "benefitType": "Adicionales",
+    "benefitQuantity": "500 MB"
+    }
+    }
+    ]
 }
